a3ffef06ce524bf4b600494587fa42cacddc807d
yomai.hiyashidewa <yomai.hiyashidewa@gmail.com>
2024-12-30 12:07:36 +0900

    パーツ追加

---
+++ sources/WorldTravelLogger/WorldTravelLogger/ViewModels/RouteRegionMiniViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldTravelLogger.ViewModels.Base;
+
+namespace WorldTravelLogger.ViewModels
+{
+    public class RouteRegionMiniViewModel : ViewModelBase
+    {
+    }
+}
